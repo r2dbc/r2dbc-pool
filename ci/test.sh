cff2c07c66286f2423991a48800cf3abf2fd3604
Greg Turnquist <gturnquist@pivotal.io>
2020-03-06 09:32:02 -0600

    Tune deployment process so releases are staged on maven central.
    Stop using bintray as the vehicle to reach maven central. bintray caches as the group level, so an r2dbc-h2 0.9.2 release ends up blocking all other modules if they are at, say, 0.9.1. Instead, stage releases directly on sonatype. Let the user manually drop.
    
    Patch maven caching so we get better leverage out of our resources.
    
    Also handle service releases.
    
    [#63]

--- ci/test.sh
+++ ci/test.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/r2dbc-h2-maven-repository" ./mvnw -P${PROFILE} clean dependency:list test -Dsort -B
+MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home" ./mvnw -P${PROFILE} clean dependency:list test -Dsort -B
